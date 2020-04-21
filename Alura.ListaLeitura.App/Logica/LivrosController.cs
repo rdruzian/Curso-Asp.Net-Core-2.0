9c8f47bb8e6e72957e99665831139e349a1ab524
rdruzian <rendruzian@gmail.com>
2020-04-21 15:17:09 -0300

    Alterações para renderizar as páginas html, correções das páginas que não estavam funcionando

--- Alura.ListaLeitura.App/Logica/LivrosController.cs
+++ Alura.ListaLeitura.App/Logica/LivrosController.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.HTML;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using System;
 using System.Linq;
@@ -18,17 +19,11 @@ namespace Alura.ListaLeitura.App.Logica
             return livro.Detalhes();
         }
 
-        public static Task ParaLer(HttpContext context)
+        public IActionResult ParaLer()
         {
-            var _repo = new LivroRepositorioCSV();
-            var conteudoArquivo = HtmlUtils.CarregaHtml("para-ler");
+            var html = new ViewResult { ViewName = "para-ler" };
 
-            foreach (var livro in _repo.ParaLer.Livros)
-            {
-                conteudoArquivo = conteudoArquivo.Replace("<tr></tr>", $"<tr><td>{livro.Titulo}</td><td>{livro.Autor}</td><td></td></tr><tr></tr>");
-            }
-
-            return context.Response.WriteAsync(conteudoArquivo);
+            return html;
         }
 
         public static Task Lendo(HttpContext context)
